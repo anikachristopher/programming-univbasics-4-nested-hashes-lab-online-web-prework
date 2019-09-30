76445425cd27d2446039a2925aa6184d7ad28d86
Anika Christopher <anika.mchristopher@gmail.com>
2019-09-30 02:47:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,24 +82,6 @@ programmer_hash =
           programmer_hash
         end
 
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-
-    
-end
 
 def changing_alan
 	programmer_hash = 
